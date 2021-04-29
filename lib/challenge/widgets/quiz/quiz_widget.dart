ba9b48c3b81689f8e6422937456112e90b3c01cc
Vitor Rondon <rondon.vitor@yahoo.com.br>
2021-04-28 22:07:35 -0300

    Aula 4

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,32 +1,50 @@
 import 'package:DevQuiz/challenge/widgets/answer/answer_widget.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/shared/models/answer_model.dart';
+import 'package:DevQuiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
-  final String title;
-  const QuizWidget({Key? key, required this.title}) : super(key: key);
+class QuizWidget extends StatefulWidget {
+  final QuestionModel question;
+  final VoidCallback onChange;
+  const QuizWidget({Key? key, required this.question, required this.onChange}) : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AnswerModel answer(int index) => widget.question.answers[index];
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
-          Text(title, style: AppTextStyles.heading),
+          SizedBox(
+            height: 64
+          ),
+          Text(
+            widget.question.title,
+            style: AppTextStyles.heading
+          ),
           SizedBox(
             height: 24,
           ),
+          for(var i = 0; i < widget.question.answers.length; i++)
           AnswerWidget(
-            isRight: false, 
-            isSelected: true,
-            title: "Possibilita a criação de aplicativos compilados nativamente" ,
+            answer: answer(i),
+            disabled: indexSelected != -1,
+            isSelected: indexSelected == i,
+            onTap: () {
+              indexSelected = i;
+              setState(() {});
+              Future.delayed(Duration(seconds: 1))
+                .then((value) => widget.onChange());
+            },
           ),
-          AnswerWidget(
-            title: "Possibilita a criação de aplicativos compilados nativamente" ,
-          ),
-          AnswerWidget(
-            title: "Possibilita a criação de aplicativos compilados nativamente" ,  
-          )
-          
         ],
       ),
     );
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:DevQuiz/challenge/challenge_page.dart';
 import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:DevQuiz/core/app_colors.dart';
 import 'package:DevQuiz/home/home_controller.dart';
@@ -68,6 +69,14 @@ class _HomePageState extends State<HomePage> {
                   controller.quizzes!.map((e) => QuizCardWidget(
                     title: e.title,
                     completed: "${e.questionAnswered}/${e.questions.length}",
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ChallengePage(questions: e.questions)
+                        )
+                      );
+                    },
                     percent: e.questionAnswered / e.questions.length
                   )).toList(),
               ),
