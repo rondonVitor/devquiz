c7becf417b98392638516363e311cea71f58ed1a
Vitor Rondon <rondon.vitor@yahoo.com.br>
2021-05-02 16:08:41 -0300

    Aula 5

--- lib/challenge/widgets/next_button/next_button_widget.dart
+++ lib/challenge/widgets/next_button/next_button_widget.dart
@@ -9,27 +9,33 @@ class NextButtonWidget extends StatelessWidget {
   final Color borderColor;
   final VoidCallback onTap;
 
-  NextButtonWidget({
-    required this.label,
-    required this.backgroundColor,
-    required this.fontColor,
-    required this.borderColor,
-    required this.onTap
-  });
+  NextButtonWidget(
+      {required this.label,
+      required this.backgroundColor,
+      required this.fontColor,
+      required this.borderColor,
+      required this.onTap});
 
   NextButtonWidget.green({required String label, required VoidCallback onTap})
-  : this.backgroundColor = AppColors.darkGreen,
-    this.fontColor = AppColors.white,
-    this.borderColor = AppColors.green,
-    this.onTap = onTap,
-    this.label = label;
+      : this.backgroundColor = AppColors.darkGreen,
+        this.fontColor = AppColors.white,
+        this.borderColor = AppColors.green,
+        this.onTap = onTap,
+        this.label = label;
+
+  NextButtonWidget.purple({required String label, required VoidCallback onTap})
+      : this.backgroundColor = AppColors.purple,
+        this.fontColor = AppColors.white,
+        this.borderColor = AppColors.green,
+        this.onTap = onTap,
+        this.label = label;
 
   NextButtonWidget.white({required String label, required VoidCallback onTap})
-  : this.backgroundColor = AppColors.white,
-    this.fontColor = AppColors.grey,
-    this.borderColor = AppColors.border,
-    this.onTap = onTap,
-    this.label = label;
+      : this.backgroundColor = AppColors.white,
+        this.fontColor = AppColors.grey,
+        this.borderColor = AppColors.border,
+        this.onTap = onTap,
+        this.label = label;
 
   @override
   Widget build(BuildContext context) {
@@ -40,14 +46,12 @@ class NextButtonWidget extends StatelessWidget {
           backgroundColor: MaterialStateProperty.all(backgroundColor),
           shape: MaterialStateProperty.all(
             RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10)
-            )
+              borderRadius: BorderRadius.circular(10),
+            ),
           ),
           side: MaterialStateProperty.all(
-            BorderSide(
-              color: borderColor
-            )
-          )
+            BorderSide(color: borderColor),
+          ),
         ),
         onPressed: onTap,
         child: Text(
@@ -55,9 +59,9 @@ class NextButtonWidget extends StatelessWidget {
           style: GoogleFonts.notoSans(
             fontWeight: FontWeight.w600,
             fontSize: 15,
-            color: fontColor
-          )
-        )
+            color: fontColor,
+          ),
+        ),
       ),
     );
   }
